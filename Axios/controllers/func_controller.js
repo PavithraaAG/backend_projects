fa1f9a101f348563aaaeca9c1e9fdcc4737c438a
Pavithraa AG <pavithraagopinath1706@gmail.com>
2023-10-03 18:49:27 +0530

    final commit

--- Axios/controllers/func_controller.js
+++ Axios/controllers/func_controller.js
@@ -3,31 +3,7 @@ const comment_model = require('../models/comment_model')
 const utils = require('../utils/utils');
 const axios = require('axios');
 
-// exports.get_comments = (req, res) => {
-//     axios.get('https://dummyjson.com/comments').then(function (response) {
-//         response.data.comments.forEach(element => {
-//             comment_model.create(element).then((data) => {
-//                 res.send("comments created successfully")
-//             })
-//         })
-//     })
-//         .catch(function (err) {
-//             console.log(err)
-//         })
-// }
-// exports.get_user_details = (req, res) => {
-//     axios.get("https://dummyjson.com/comments").then(function (response) {
-//         response.data.comments.forEach(element => {
-//             console.log(element.user)
-//             user_model.create({ user: element.user }).then(() => {
-//                 res.send("Users inserted successfully")
-//             })
-//         })
-//     })
-//         .catch(function (err) {
-//             console.log(err)
-//         })
-// }
+
 
 exports.get_comments = async (req, res) => {
     let response = await utils.sendRequest("comments")
@@ -39,20 +15,22 @@ exports.get_comments = async (req, res) => {
     response.data.comments.forEach(async (val) => {
         console.log(val)
         let commentId = val.id
-        let body= val.body
-        let postId=val.postId
-        let user=val.user
+        let body = val.body
+        let postId = val.postId
+        let user = val.user
         console.log(commentId)
-        let data = await comment_model.find({ commentId : commentId })
+        let data = await comment_model.find({ commentId: commentId })
         console.log("hi", data)
         if (data.length === 0) {
-            await comment_model.create({commentId: commentId,
+            await comment_model.create({
+                commentId: commentId,
                 body: body,
                 postId: postId,
-                user: user})
+                user: user
+            })
         }
     })
-    
+
     res.send("Comment data inserted successfully")
 
 }
